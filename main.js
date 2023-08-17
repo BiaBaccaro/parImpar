7eede741859b374e10be6e0249131fc96947c6c3
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-08-17 14:06:16 -0300

    commit bia

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-var number = par;
-var number = impar;
+document.addEventListener("DOMContentLoaded", function () {
+    const numberInput = document.getElementById("numberInput");
+    const checkButton = document.getElementById("checkButton");
+    const result = document.getElementById("result");
+
+    checkButton.addEventListener("click", function () {
+        const inputValue = parseInt(numberInput.value);
+
+        if (isNaN(inputValue)) {
+            result.textContent = "Por favor, insira um valor.";
+        } else {
+            if (inputValue % 2 === 0) {
+                result.textContent = "O número é par.";
+            } else {
+                result.textContent = "O número é ímpar.";
+            }
+        }
+    });
+});
